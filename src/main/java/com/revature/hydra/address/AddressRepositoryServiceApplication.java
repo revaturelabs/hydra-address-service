e99269139f36ff7de6f9c94d6120a75150fc43d1
Bryton Hall <email@bryton.io>
2018-04-01 23:44:50 -0400

    seperate documentation config from main class

--- src/main/java/com/revature/hydra/address/AddressRepositoryServiceApplication.java
+++ src/main/java/com/revature/hydra/address/AddressRepositoryServiceApplication.java
@@ -2,29 +2,16 @@ package com.revature.hydra.address;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableSwagger2
-@EnableEurekaClient
+@EnableDiscoveryClient
+@ComponentScan
 public class AddressRepositoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressRepositoryServiceApplication.class, args);
 	}
 
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.revature.hydra.controller")).paths(PathSelectors.any())
-				.build();
-	}
 }

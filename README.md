c274ba65f6760d3dbba27dd3762e309fedbf50d7
Bryton Hall <email@bryton.io>
2018-04-06 16:51:56 -0400

    overhaul service

--- README.md
+++
@@ -1,5 +0,0 @@
-# hydra-address-service
-
-The service creates, reads, updates and deactivates addresses.  
-The address contains street, city, state, zipcode, company and active status.  
-
---
+++ src/main/java/com/revature/hydra/address/repo/AddressRepo.java
@@ -0,0 +1,11 @@
+package com.revature.hydra.address.repo;
+
+import com.revature.hydra.address.model.Address;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AddressRepo extends JpaRepository<Address, Integer> {
+}

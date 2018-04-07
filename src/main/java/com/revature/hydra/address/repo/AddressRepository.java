87c931db38b921805e6588b1543fde057c137028
Robert Hamel <roberthamel@gmail.com>
2018-04-06 23:14:06 -0400

    change Jpa to Crud repository

--- src/main/java/com/revature/hydra/address/repo/AddressRepository.java
+++ src/main/java/com/revature/hydra/address/repo/AddressRepository.java
@@ -1,11 +1,10 @@
 package com.revature.hydra.address.repo;
 
 import com.revature.hydra.address.model.Address;
-
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface AddressRepository extends JpaRepository<Address, Integer> {
+public interface AddressRepository extends CrudRepository<Address, Integer> {
 }
